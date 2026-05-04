b64ab1d69e5dd3bd28ab24b61e009ff1c63f7578
kericheouafa <ouafakeriche@gmail.com>
2026-05-04 23:38:14 +0200

    J8 - Formulaire ajout, routing, Angular Material

--- src/app/ajouter/ajouter.ts
+++ src/app/ajouter/ajouter.ts
@@ -4,13 +4,20 @@ import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
+import { TachesService } from '../taches/taches.service';
 
 
 
 @Component({
   selector: 'app-ajouter',
-  imports: [MatFormField ,MatSelectModule, MatDatepickerModule , MatInputModule , MatFormFieldModule ],
+  imports: [FormsModule, MatFormField ,MatSelectModule, MatDatepickerModule , MatInputModule , MatFormFieldModule ],
   templateUrl: './ajouter.html',
   styleUrl: './ajouter.css',
 })
-export class Ajouter {}
+export class Ajouter {
+  titre: string = '';
+priorite: number = 1;
+dateLimite: Date | null = null;
+constructor(private tachesService: TachesService) {}
+}
