b6da95345861014f1f49f8475c0071023e0e2ef9
kericheouafa <ouafakeriche@gmail.com>
2026-05-26 20:12:20 +0200

    J13 - Guards Angular + protection routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,13 @@
 import { Routes } from '@angular/router';
 import { Accueil } from './accueil/accueil';
-
 import { Taches } from './taches/taches';
 import { Ajouter } from './ajouter/ajouter';
 import { LoginComponent } from './login/login';
-
+import { authGuard } from './services/auth-guard';
 
 export const routes: Routes = [
-{ path: '', component: Accueil },
+  { path: '', component: Accueil },
   { path: 'login', component: LoginComponent },
-{ path: 'taches', component: Taches },
-{ path: 'ajouter', component: Ajouter }        
-                              ];
+  { path: 'taches', component: Taches, canActivate: [authGuard] },
+  { path: 'ajouter', component: Ajouter, canActivate: [authGuard] }
+];
